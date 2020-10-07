628741ec105a93257bc9bf00f759a03fca53f744
Kacper Marzecki <60017757+kacper-marzecki@users.noreply.github.com>
2020-10-08 00:25:53 +0200

    added basic navigation skeleton

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { ReactElement, useState } from 'react';
 import './App.css';
-import {faTerminal} from '@fortawesome/free-solid-svg-icons'
-import {AppViewPort} from "./AppViewPort";
+import { faTerminal } from '@fortawesome/free-solid-svg-icons';
+import { AppViewPort } from './AppViewPort';
+import { CommandPage } from './CommandPage';
 
+type Page = 'command';
+
+interface AppState {
+  //TODO
+  commandState: {};
+  selectedPage: Page;
+}
 
 function App() {
+  const [ state, setState ] = useState<AppState>({ commandState: {}, selectedPage: 'command' });
+  const setPage = (page: Page) => () => {
+    setState((s) => ({ ...s, selectedPage: page }));
+  };
   const elements = [
     {
       icon: faTerminal,
-      onClick: () => {
-      }
+      onClick: setPage('command')
     }
   ];
-  return (
-    <AppViewPort elements={elements}>
-    </AppViewPort>
-  );
+  const selectedPage: ReactElement | undefined = (() => {
+    switch (state.selectedPage) {
+      case 'command':
+        return <CommandPage commandState={state.commandState} />;
+    }
+  })();
+  return <AppViewPort elements={elements}>{selectedPage}</AppViewPort>;
 }
 
 export default App;
--- ui/src/AppViewPort.tsx
+++ ui/src/AppViewPort.tsx
@@ -1,57 +1,48 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPowerOff} from "@fortawesome/free-solid-svg-icons/faPowerOff";
-import React, {FunctionComponent} from "react";
-import {IconDefinition} from "@fortawesome/fontawesome-common-types";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons/faPowerOff';
+import React, { FunctionComponent } from 'react';
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
 
 export interface NavbarElement {
-  icon: IconDefinition,
-  onClick: () => void
+  icon: IconDefinition;
+  onClick: () => void;
 }
 
 interface Props {
-  elements: NavbarElement[]
+  elements: NavbarElement[];
 }
 
-export const AppViewPort: FunctionComponent<Props> = ({elements, children}) => {
-
+export const AppViewPort: FunctionComponent<Props> = ({ elements, children }) => {
   return (
     <div className="flex flex-row h-full">
       <nav className="bg-gray-900 w-20  justify-between flex flex-col ">
         <div className="mt-10 mb-10">
           <a href="#">
-            <p className="font-mono text-lg text-white text-center">
-              ctool
-            </p>
+            <p className="font-mono text-lg text-white text-center">ctool</p>
           </a>
           <div className="mt-10">
             <ul>
               {elements.map((it, idx) => (
                 <li key={idx} className="mb-6 mx-auto fill-current h-5 w-5 mx-auto text-gray-300 hover:text-green-500">
                   <a href="#">
-              <span>
-                <FontAwesomeIcon
-                  onClick={it.onClick}
-                  icon={it.icon}/>
-              </span>
+                    <span>
+                      <FontAwesomeIcon onClick={it.onClick} icon={it.icon} />
+                    </span>
                   </a>
                 </li>
               ))}
-
             </ul>
           </div>
         </div>
         <div className="mb-4 mx-auto hover:color-green-500">
           <a href="#">
-          <span>
-            <FontAwesomeIcon className="fa-grey "
-                             icon={faPowerOff}/>
-          </span>
+            <span>
+              <FontAwesomeIcon className="fa-grey " icon={faPowerOff} />
+            </span>
           </a>
         </div>
       </nav>
-      <div className="px-16 py-4 text-gray-700 bg-gray-200 h-screen w-screen">
-        {children}
-      </div>
+      <div className="px-16 py-4 text-gray-700 bg-gray-200 h-screen w-screen">{children}</div>
     </div>
   );
-}
+};
